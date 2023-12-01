8f4872fa6e5d461bac7eed028d71f2a3e851191a
root <root@LAPTOP-OCMDPFCO>
2023-12-01 12:53:22 -0500

    despliegue_3

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
-source env/bin/activate
-pip install --upgrade pip
+# source env/bin/activate
+# pip install --upgrade pip
 pip install -r requirements.txt
 reflex init
 reflex export --frontend-only
 rm -rf public
 unzip frontend.zip -d public
 rm -f frontend.zip
-deactivate
+# deactivate
--- rxconfig.py
+++ rxconfig.py
@@ -2,5 +2,4 @@ import reflex as rx
 
 config = rx.Config(
     app_name="STC",
-    db_url="sqlite:///reflex.db",
 )
